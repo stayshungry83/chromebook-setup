a6f5c4600d51171f0412b97116b7edcb0d3b2301
Conail Stewart <conail@users.noreply.github.com>
2015-04-08 19:19:14 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
 # Use crouton to install core target.
 sudo sh crouton -t core
 
-sudo apt-get install build-essential git-Core vim emacs screen tmux zsh tree htop
+sudo apt-get install build-essential git-core vim emacs screen tmux zsh tree htop curl wget libssl-dev zlib1g-dev
+
+# Ohmyzsh
+wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+
+# Node
+wget http://nodejs.org/dist/v0.11.9/node-v0.11.9.tar.gz 
+tar -zxvf node-v0.11.9.tar.gz 
+cd node-v0.11.9 
+./configure --without-snapshot --dest-cpu=arm --dest-os=linux 
+make 
+sudo make install 
+hash -r
